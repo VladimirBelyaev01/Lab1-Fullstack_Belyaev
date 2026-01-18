63ecd2a1c304140e9b22734fd31157f69a10c949
VladimirBelyaev01 <volodya.belyaev.06@bk.ru>
2026-01-18 21:46:38 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Беляев В.А.");
+Console.WriteLine($"Сейчас: {DateTime.Now}");
