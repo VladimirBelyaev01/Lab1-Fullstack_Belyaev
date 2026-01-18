2db37ca86a6ae5fd7c35b10c28bd9dd701e77ffd
VladimirBelyaev01 <volodya.belyaev.06@bk.ru>
2026-01-18 22:53:32 +0300

    Заверишл логику

--- backend/program.cs
+++ backend/program.cs
@@ -1,8 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Concurrent;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Reflection;
 using System.Reflection.Emit;
+using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
 using System.Runtime.Serialization;
 using System.Text;
@@ -13,3 +20,19 @@ Console.WriteLine($"Сейчас: {DateTime.Now}");
 Console.Write("Введите имя ");
 string Name = Console.ReadLine();
 Console.WriteLine($"Привет {Name}");
+
+string day = DateTime.Now.DayOfWeek.ToString();
+Console.WriteLine("Сегодня: {day}");
+
+Console.Write("Хочешь продолжить? (y/n)");
+string prod = Console.ReadLine().ToLower();
+
+if (prod=="y || prod == "yes" || prod == "da")
+{
+    Console.WriteLine("Продолжим..")
+    Console.WriteLine("$Твое имя состоит из {Name.Length} букв");
+}
+else
+{
+    Console.WriteLine("Прога заверешна");
+}
