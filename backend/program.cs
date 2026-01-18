8ee92fb654766803e1f6545fd3eb28c8b0173278
VladimirBelyaev01 <volodya.belyaev.06@bk.ru>
2026-01-18 22:45:15 +0300

    Добавил имя

--- backend/program.cs
+++ backend/program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Runtime.Serialization;
+using System.Text;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Меня зовут Беляев В.А.");
 Console.WriteLine($"Сейчас: {DateTime.Now}");
+Console.Write("Введите имя ");
+string Name = Console.ReadLine();
+Console.WriteLine($"Привет {Name}");
